20f1d80b6686f1b5edd4160d317e8303122341ec
ldover <luka.dover@gmail.com>
2024-09-16 17:08:03 +0200

    Add missing exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,7 +8,7 @@ export {
   type TreeOptions
 } from './tree.js'
 
-export { SvelteList, SvelteListItem, type List, type ListItem } from './list.js'
+export { SvelteList, SvelteListItem, type List, type ListItem, ListSelection, SelectionRange } from './list.js'
 
 export { default as SvelteTreeNodeUI } from './SvelteTreeNodeUI.svelte'
 export { default as SvelteTreeUI } from './SvelteTreeUI.svelte'
